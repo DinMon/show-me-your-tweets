c059aeca4778eba823035e6ea4e5b097ca4413af
Dinys <dinysmon2013>
2020-09-13 12:39:29 +1000

    Add React and TailwindCSS

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-4xl font-bold text-center text-blue-500">Hello World!</h1>
+    </div>
+  );
+}
+
+export default App;
