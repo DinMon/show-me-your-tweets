00464d77dfc7d4a04078098f6fbacdf80ad919f2
Dinys <dinysmon2013>
2020-09-15 16:02:24 +1000

    Remove unuse hook

--- src/hooks/useHighlightText.js
+++
@@ -1,6 +0,0 @@
-import {useState} from 'react'
-
-export default function useHighlightText(text, textHighlighted) {
-    const [start, setStart] = useState('')
-    const [end, setEnd] = useState('')
-}
