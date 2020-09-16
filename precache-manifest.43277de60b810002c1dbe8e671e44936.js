8a583b6838ee247c0f4afb701e001938ff97e43d
Dinys <dinysmon2013>
2020-09-17 08:20:16 +1000

    Updates

---
+++ precache-manifest.43277de60b810002c1dbe8e671e44936.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0d3f2fb8ea049c246501c4e9204dafc",
+    "url": "/show-me-your-tweets/index.html"
+  },
+  {
+    "revision": "e28c74dc2c55b667d370",
+    "url": "/show-me-your-tweets/static/css/main.dedb8df7.chunk.css"
+  },
+  {
+    "revision": "ee29c8e887dac4a431c4",
+    "url": "/show-me-your-tweets/static/js/2.51a1f06f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/show-me-your-tweets/static/js/2.51a1f06f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e28c74dc2c55b667d370",
+    "url": "/show-me-your-tweets/static/js/main.3f3cc265.chunk.js"
+  },
+  {
+    "revision": "0e573fd4c28e16cddc56",
+    "url": "/show-me-your-tweets/static/js/runtime-main.3d0e719d.js"
+  }
+]);
